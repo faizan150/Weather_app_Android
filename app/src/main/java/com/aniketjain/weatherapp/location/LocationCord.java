88e8fd56d8e2db97a8b9b0e7d90016fc207b844b
Muhammad Faizan <faizi12570@gmail.com>
2023-06-09 22:40:49 +0500

    full prject Weather App in Android Studio

---
+++ app/src/main/java/com/aniketjain/weatherapp/location/LocationCord.java
@@ -0,0 +1,10 @@
+package com.aniketjain.weatherapp.location;
+
+public class LocationCord {
+    public static String lat = "33.6844";
+    public static String lon = "73.0479";
+    //    public final static String API_KEY = "AIzaSyB8RIo5vfgpcgk2WUryOyNRoXrlrFaaD1s";
+    public final static String API_KEY = "eeb8b40367eee691683e5a079e2fa695";
+
+}
+
